4d9bb92e86840a17b9c45ae6a92bf48fb54c5c72
Acendy <zentokaishi@gmail.com>
2018-12-03 16:10:30 +0100

    Ajout de la barre de navigation en bas

---
+++ app/src/main/java/com/example/tom/projet_if26/Exercices.java
@@ -0,0 +1,33 @@
+package com.example.tom.projet_if26;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Exercices extends Fragment {
+
+    private ExercicesViewModel mViewModel;
+
+    public static Exercices newInstance() {
+        return new Exercices();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.exercices_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ExercicesViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
