43eb90540e69345227a7f8718b346392a006808e
Acendy <zentokaishi@gmail.com>
2019-01-21 18:30:09 +0100

    Quelques modifications visuelles

--- app/src/main/java/com/example/tom/projet_if26/Propos.java
+++ app/src/main/java/com/example/tom/projet_if26/Propos.java
@@ -1,9 +1,16 @@
 package com.example.tom.projet_if26;
 
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Propos extends AppCompatActivity {
+    private ImageView banniere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,14 @@ public class Propos extends AppCompatActivity {
         setContentView(R.layout.activity_propos);
         getSupportActionBar().setTitle("À propos");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        banniere = findViewById(R.id.banniere);
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream ims = assetManager.open("fitmeetlogo.png");
+            Drawable d = Drawable.createFromStream(ims, null);
+            banniere.setImageDrawable(d);
+        } catch (IOException e) {
+            return;
+        }
     }
 }
